a81c60180eaff243fb5a130a176c57f522eefcee
tmartin <timcmartin@gmail.com>
2015-06-08 14:36:02 -0600

    Testing Properties files.

---
+++ properties/local.update_dotfiles.pp
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+  "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>local.update_dotfiles</string>
+    <key>Program</key>
+    <string></Users/timcmartin/Dotfiles/subcron.sh/string>
+    <key>WorkingDirectory</key>
+    <string>/Users/timcmartin/Dotfiles</string>
+    <key>StartCalendarInterval</key>
+    <key>KeepAlive</key>
+    <true/>
+    <dict>
+        <key>Hour</key>
+        <integer>7</integer>
+        <key>Minute</key>
+        <integer>0</integer>
+    </dict>
+</dict>
+</plist>
+
